230d6921ae549ab5882271ddf5334ad7299d2abc
Colheen0 <colheenn@gmail.com>
2024-11-29 10:06:00 +0100

    changement php

--- index.php
+++ index.php
@@ -6,6 +6,14 @@
     <title>Ent Accueil</title>
 </head>
 <body>
-    
+    <h1>Widget accueil</h1>
+
+    <h2>widget absences</h2>
+    <div class="petit-widget">
+        <h2></h2>
+        <img src="" alt="">
+        
+    </div>
+
 </body>
 </html>
